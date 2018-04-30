15ec8986c5e37d5a364088478fb63b5c9623e00e
Forrest Olson <forrest@odotmedia.com>
2018-04-30 11:40:41 -0500

    Refactoring

--- js/app.js
+++ js/app.js
@@ -1,123 +1,179 @@
-// function to create new element
-const createElement = (element, prop1, val1, prop2, val2, ) => {
-    let newElement = document.createElement(element);
-    newElement[prop1] = val1;
-    newElement[prop2] = val2;
-    return newElement;
-}
-
-// function to hide element on the page
-const hideElement = ( element ) => {
-    element[`style`][`display`] = `none`;
-}
-
-// function to show the element
-// accepts 2 arguments:
-//  1. element = element we want to show
-//  2. value = how we want to display element (block, flex, etc..)
-const showElement = ( element, value ) => {
-    element[`style`][`display`] = value ;
-}
-
-// Variables
-const form = document.querySelector(`form`);
-const name = document.querySelector(`#name`);
-const email = document.querySelector(`input[type=email]`);
-const userTitle = document.querySelector(`select#title`);
-const designSelect = document.querySelector(`select#design`);
-const colorDiv = document.querySelector(`#color-div`);
-const activities = document.querySelector(`.activities`);
-const activityCheckboxes = activities.querySelectorAll(`input[type=checkbox]`);
-const paymentDiv = document.querySelector(`#payment-section`);
-const paymentSelect = document.querySelector(`#payment`);
-const ccDiv = paymentDiv.querySelector(`#credit-card`);
-const ccNum = paymentDiv.querySelector(`#cc-num`);
-const ccZip = paymentDiv.querySelector(`#zip`);
-const ccCVV = paymentDiv.querySelector(`#cvv`);
-const paypal = paymentDiv.querySelector(`#paypal`);
-const bitcoin = paymentDiv.querySelector(`#bitcoin`);
-const tuesday1 = [];
-const tuesday9 = [];
-let cost = 0;
-const costP = document.querySelector(`.total-cost`);
-const costSpan = createElement(`span`, `class`, `cost`, `textContent`, cost);
-costP.appendChild(costSpan);
-
-const addToCost = ( number ) => {
-    cost += number;
-    costSpan.textContent = cost;
-}
-
-const subtractFromCost = ( number ) => {
-    cost -= number;
-    costSpan.textContent = cost;
-}
-
-const addInputValidation = ( element ) => {
-    element.addEventListener(`input`, () => {
-        let parent = element.parentNode;
-        let parentClass = parent.className;
-        if ( element.validity.valid ) {
-            if ( parentClass === `col-6 col` ) {
-                parent.classList.remove(`invalid`);
-                parent.classList.add(`valid`);
-                console.log(`contains col`);
-            } else if ( parentClass === `col-3 col` ) {
-                parent.classList.remove(`invalid`);
-                parent.classList.add(`valid`);
-            } else {
-                parent.classList.remove(`invalid`);
-                parent.classList.add(`valid`);
-            }
-        } else if ( !element.validity.valid ) {
-            if ( parentClass === `col-6 col` ) {
-                parent.classList.remove(`valid`);
-                parent.classList.add(`invalid`);
-                console.log(`contains col`);
-            } else if ( parentClass === `col-3 col` ) {
-                parent.classList.remove(`valid`);
-                parent.classList.add(`invalid`);
-            } else {
-                parent.classList.remove(`valid`);
-                parent.classList.add(`invalid`);
-            }
-        }
-
-    });
-}
-
-// -------------------------------------
-// FORM SUBMIT EVENT HANDLER
-// -------------------------------------
-
-form.addEventListener(`submit`, ( e ) => {
-    e.preventDefault();
-    let designValue = designSelect.value;
-    let designLabel = document.querySelector(`label[for=design]`);
-    if ( designValue === `Select Theme` ) {
-        console.log(designLabel);
-        designLabel.textContent = `Design: Please select a theme`;
-        designLabel.style.color = `#ee5253`;
-    }
-});
-
+// on document load:
 document.addEventListener(`DOMContentLoaded`, () => {
 
-    // add focus to name input on load
+    //===============================
+    // Variables
+    //===============================
+    // the <form> element
+    const form = document.getElementsByTagName(`form`)[0];
+    // the name <input> field
+    const name = document.querySelector(`#name`);
+    // the email <input> field
+    const email = document.querySelector(`input[type=email]`);
+    // the job title <select>
+    const userTitle = document.querySelector(`select#title`);
+    // the other job description <input>
+    const otherJobInput = document.querySelector(`.other-job`);
+    // the design <select>
+    const designSelect = document.querySelector(`select#design`);
+    // the <div> housing the color <label> & <select>
+    const colorDiv = document.querySelector(`#color-div`);
+    // the activities <fieldset>
+    const activities = document.querySelector(`.activities`);
+    // all of the activity <input> checkboxes
+    const activityCheckboxes = activities.querySelectorAll(`input[type=checkbox]`);
+    // the payment <fieldset>
+    const paymentDiv = document.querySelector(`#payment-section`);
+    // the payment <select>
+    const paymentSelect = document.querySelector(`#payment`);
+    // the cc <div>
+    const ccDiv = paymentDiv.querySelector(`#credit-card`);
+    // the cc number <input>
+    const ccNum = paymentDiv.querySelector(`#cc-num`);
+    // the cc zip code <input>
+    const ccZip = paymentDiv.querySelector(`#zip`);
+    // the cc CVV code <input>
+    const ccCVV = paymentDiv.querySelector(`#cvv`);
+    // the paypal payment information
+    const paypal = paymentDiv.querySelector(`#paypal`);
+    // the bitcoin payment inpformation
+    const bitcoin = paymentDiv.querySelector(`#bitcoin`);
+    // empty array for activities on Tuesday @ 1PM
+    const tuesday1 = [];
+    // empty array for activities on Tuesday @ 9AM
+    const tuesday9 = [];
+    // the value of the cost of the activities
+    let cost = 0;
+    const costP = document.querySelector(`.total-cost`);
+    const costSpan = createElement(`span`, `class`, `cost`, `textContent`, cost);
+    costP.appendChild(costSpan);
+    const registerBtn = document.querySelector(`button[type=submit]`);
+
+
+    //===============================
+    // Functions
+    //===============================
+
+    // function to create new element
+    const createElement = ( element, prop1, val1, prop2, val2 ) => {
+        let newElemenet = document.createElement(element);
+        newElement[prop1] = val1;
+        newElement[prop2] = val2;
+        return newElement;
+    }
+
+    // function to hide element on the page
+    const hideElement = ( element ) => {
+        element[`style`][`display`] = `none`;
+    }
+
+    // funciton to show the Element
+    // accepts 2 arguments:
+    // 1. element = element we want to show
+    // 2. value = how we want to display element (block, flex, etc...)
+    const showElement = ( element, value ) => {
+        element[`style`][`display`] = value;
+    }
+
+    // function to add to cost for activities
+    const addToCost = ( number ) => {
+        cost += number;
+        costSpan.textContent = cost;
+    }
+
+    // function to subtract from cost for activities
+    const subtractFromCost = ( number ) => {
+        cost -= number;
+        costSpan.textContent = cost;
+    }
+
+    // adding real-time input validation on <input> fields
+    const addInputValidation = ( element ) => {
+        element.addEventListener(`input`, () => {
+            let parent = element.parentNode;
+            let parentClass = parent.className;
+            // REFACTORING NOTES:
+            // 1. create function to remove invalid and add valid
+            // 2. create function to remove valid and add invalid
+            if ( element.validity.valid ) {
+                if ( parentClass === `col-6 col` ) {
+                    parent.classList.remove(`invalid`);
+                    parent.classList.add(`valid`);
+                } else if ( parentClass === `col-3 col` ) {
+                    parent.classList.remove(`invalid`);
+                    parent.classList.add(`valid`);
+                } else {
+                    parent.classList.remove(`invalid`);
+                    parent.classList.add(`valid`);
+                }
+            } else if ( !element.validity.valid ) {
+                if ( parentClass === `col-6 col` ) {
+                    parent.classList.remove(`valid`);
+                    parent.classList.add(`invalid`);
+                    console.log(`contains col`);
+                } else if ( parentClass === `col-3 col` ) {
+                    parent.classList.remove(`valid`);
+                    parent.classList.add(`invalid`);
+                } else {
+                    parent.classList.remove(`valid`);
+                    parent.classList.add(`invalid`);
+                }
+            }
+        });
+    }
+
+    // putting focus on the name <input>
+    // -- non-JS fallback: autofocus attribute on element
     name.focus();
 
+    // adding the real-time <input> validation
     addInputValidation(name);
     addInputValidation(email);
 
-    // hiding the color div
+    // hiding the color <div>
     if ( designSelect.value === `Select Theme` ) {
         colorDiv.className = `is-hidden`;
     }
 
-    // hiding cc, paypal, & bitcoin
+    // hiding the other job title <input>
+    hideElement(otherJobInput);
+
+    // hiding the cc, paypal, & bitcoin <div>s
     hideElement(ccDiv);
     hideElement(paypal);
     hideElement(bitcoin);
+
+    // hide the cost
+    hideElement(costP);
+
+});
+
+// -------------------------------------
+// form submit validation
+// -------------------------------------
+
+form.addEventListener( `submit`, (e) => {
+    e.preventDefault();
+    formSubmitValidation();
+});
+
+document.addEventListener(`DOMContentLoaded`, () => {
+
+    // hiding the color div
+    // if ( designSelect.value === `Select Theme` ) {
+    //     colorDiv.className = `is-hidden`;
+    // }
+
+    // hide the other input other input
+    // hideElement(otherJobInput);
+    //
+    // // hiding cc, paypal, & bitcoin
+    // hideElement(ccDiv);
+    // hideElement(paypal);
+    // hideElement(bitcoin);
+
+    // hiding the cost of the activities
+    hideElement(costP);
 
     // sorting activities
     for ( let i = 0; i < activityCheckboxes.length; i++ ) {
@@ -131,8 +187,6 @@ document.addEventListener(`DOMContentLoaded`, () => {
         }
     }
 
-    hideElement(costP);
-
     // -------------------------------------
     // FORM CHANGE EVENT HANDLER
     // -------------------------------------
@@ -144,18 +198,12 @@ document.addEventListener(`DOMContentLoaded`, () => {
             let value = e.target.value;
             // if the value is `other` then:
             if ( value.toLowerCase() === `other` ) {
-                // create a new input
-                let otherInput = createElement(`input`, `type`, `text`, `placeholder`, `Your job here`);
-                // give it a class name
-                otherInput.className = `other-job`;
-                // select the fieldset
-                let basicInfo = document.querySelector(`.basic-info`);
-                basicInfo.appendChild(otherInput);
+                // OTHERINPUT
+                showElement(otherJobInput, `block`);
+
             } else if ( value.toLowerCase() !== `other` ) {
-                let inputToRemove = document.querySelector(`.other-job`);
-                if ( inputToRemove ) {
-                    let basicInfo = document.querySelector(`.basic-info`);
-                    basicInfo.removeChild(inputToRemove);
+                if ( otherJobInput ) {
+                    hideElement(otherJobInput);
                 }
             }
         }
@@ -204,41 +252,8 @@ document.addEventListener(`DOMContentLoaded`, () => {
 
         }
 
-        // PAYMENT SECTION
-        if ( e.target.id === paymentSelect.id ) {
-
-            let ccInputs = ccDiv.children;
-            console.log(ccInputs.length);
-
-            if ( e.target.value === `credit card` ) {
-                showElement(ccDiv, `block`);
-                addInputValidation(ccNum);
-                addInputValidation(ccZip);
-                addInputValidation(ccCVV);
-                hideElement(paypal);
-                hideElement(bitcoin);
-                console.log(`credit card info shown`);
-            } else if ( e.target.value === `paypal` ) {
-                showElement(paypal, `block`);
-                hideElement(ccDiv);
-                hideElement(bitcoin);
-                console.log(`paypal info shown`);
-            } else if ( e.target.value === `bitcoin` ) {
-                showElement(bitcoin, `block`);
-                hideElement(ccDiv);
-                hideElement(paypal);
-                console.log(`bitcoin info shown`);
-            } else {
-                hideElement(ccDiv);
-                hideElement(paypal);
-                hideElement(bitcoin);
-                console.log(`all payment divs hidden`);
-            }
-        }
-
-
         //-------------------------------------
-        // SO MUCH ROOM FOR ACTIVITIES!!!
+        // Activities
         //-------------------------------------
 
         if ( e.target.type === `checkbox` ) {
@@ -253,18 +268,22 @@ document.addEventListener(`DOMContentLoaded`, () => {
 
                 if ( e.target.name === `js-frameworks`) {
                     tuesday9[1].disabled = `true`;
+                    tuesday9[1].parentNode.style.color = `gray`;
                     addToCost(100);
                     console.log(`express disabled`);
                 } else if ( e.target.name === `express` ) {
                     tuesday9[0].disabled = `true`;
+                    tuesday9[0].parentNode.style.color = `gray`;
                     addToCost(100);
                     console.log(`frameworks disabled`);
                 } else if ( e.target.name === `node` ) {
                     tuesday1[0].disabled = `true`;
+                    tuesday1[0].parentNode.style.color = `gray`;
                     addToCost(100);
                     console.log(`js-libs disabled`);
                 } else if ( e.target.name === `js-libs`) {
                     tuesday1[1].disabled = `true`;
+                    tuesday1[1].parentNode.style.color = `gray`;
                     addToCost(100);
                     console.log(`node disabled`);
                 } else if ( e.target.name === `all` ) {
@@ -280,21 +299,25 @@ document.addEventListener(`DOMContentLoaded`, () => {
                 if ( e.target.name === `js-frameworks` ) {
                     for ( let i = 0; i < tuesday9.length; i++ ) {
                         tuesday9[i].disabled = false;
+                        tuesday9[i].parentNode.style.color = `#000`;
                     }
                     subtractFromCost(100);
                 } else if ( e.target.name === `express` ) {
                     for ( let i = 0; i < tuesday9.length; i++ ) {
                         tuesday9[i].disabled = false;
+                        tuesday9[i].parentNode.style.color = `#000`;
                     }
                     subtractFromCost(100);
                 } else if ( e.target.name === `js-libs` ) {
                     for ( let i = 0; i < tuesday1.length; i++ ) {
                         tuesday1[i].disabled = false;
+                        tuesday1[i].parentNode.style.color = `#000`;
                     }
                     subtractFromCost(100);
                 } else if ( e.target.name === `node` ) {
                     for ( let i = 0; i < tuesday1.length; i++ ) {
                         tuesday1[i].disabled = false;
+                        tuesday1[i].parentNode.style.color = `#000`;
                     }
                     subtractFromCost(100);
                 } else if ( e.target.name === `all` ) {
@@ -304,6 +327,37 @@ document.addEventListener(`DOMContentLoaded`, () => {
                 } else if ( e.target.name === `npm` ) {
                     subtractFromCost(100);
                 }
+            }
+        }
+
+        // PAYMENT SECTION
+        if ( e.target.id === paymentSelect.id ) {
+
+            let ccInputs = ccDiv.children;
+
+            if ( e.target.value === `credit card` ) {
+                showElement(ccDiv, `block`);
+                addInputValidation(ccNum);
+                addInputValidation(ccZip);
+                addInputValidation(ccCVV);
+                ccNum.required = true;
+                ccZip.required = true;
+                ccCVV.required = true;
+                hideElement(paypal);
+                hideElement(bitcoin);
+            } else if ( e.target.value === `paypal` ) {
+                showElement(paypal, `block`);
+                hideElement(ccDiv);
+                hideElement(bitcoin);
+            } else if ( e.target.value === `bitcoin` ) {
+                showElement(bitcoin, `block`);
+                hideElement(ccDiv);
+                hideElement(paypal);
+            } else {
+                hideElement(ccDiv);
+                hideElement(paypal);
+                hideElement(bitcoin);
+                console.log(`all payment divs hidden`);
             }
         }
     });
